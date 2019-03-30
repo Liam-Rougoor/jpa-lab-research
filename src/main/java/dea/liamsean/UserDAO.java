8b30d482a5f865cfaeeeec1c8e3526f5dcd3c8b2
Liam-Rougoor <liam.rougoor@hotmail.nl>
2019-03-30 17:50:51 +0100

    Creating users works

---
+++ src/main/java/dea/liamsean/UserDAO.java
@@ -0,0 +1,29 @@
+package dea.liamsean;
+
+import javax.persistence.EntityManager;
+import javax.persistence.Persistence;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+import java.util.List;
+
+public class UserDAO {
+
+    private EntityManager entityManager;
+
+    public User createUser(User user){
+        entityManager = createNewEntityManager();
+        entityManager.persist(user);
+        entityManager.flush();
+        return user;
+    }
+
+    public List<User> getAllUsers(){
+        entityManager = createNewEntityManager();
+        Query query = entityManager.createQuery("SELECT u FROM User u");
+        return query.getResultList();
+    }
+
+    private EntityManager createNewEntityManager(){
+        return Persistence.createEntityManagerFactory("persistenceUnit").createEntityManager();
+    }
+}
--- src/main/java/dea/liamsean/UserResource.java
+++ src/main/java/dea/liamsean/UserResource.java
@@ -1,26 +1,48 @@
 package dea.liamsean;
 
+import javax.ejb.Stateless;
 import javax.inject.Inject;
 import javax.persistence.*;
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
+import javax.transaction.Transactional;
+import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import java.util.List;
 
+@Stateless
+@Transactional
 @Path("/")
 public class UserResource {
 
-    @PersistenceContext(unitName = "persistenceUnit")
-    private EntityManager entityManager;
+    UserDAO userDAO = new UserDAO();
 
     @GET
     @Produces(MediaType.APPLICATION_JSON)
     public Response getAllUsers(){
-        entityManager = Persistence.createEntityManagerFactory("persistenceUnit").createEntityManager();
-        Query query = entityManager.createQuery("SELECT u FROM User u");
-        List<User> users = query.getResultList();
+        List<User> users = userDAO.getAllUsers();
         return Response.ok(users).build();
+    }
+
+    @GET
+    @Path("POSTING")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response postRandomUser(){
+        User user = new User();
+        user.setUsername("tienepien");
+        user.setPassword("tienePass");
+        user.setName("Jantiene");
+        EntityManager entityManager = Persistence.createEntityManagerFactory("persistenceUnit").createEntityManager();
+        entityManager.joinTransaction();
+        entityManager.persist(user);
+        entityManager.flush();
+        return Response.status(Response.Status.CREATED).entity(user).build();
+    }
+
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response addUser(User user){
+        User createdUser = userDAO.createUser(user);
+        return Response.status(Response.Status.CREATED).entity(createdUser).build();
     }
 }
