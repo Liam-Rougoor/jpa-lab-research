f29e8d9b8595a963dd0c50cb3d781da1f4f8a108
Liam-Rougoor <liam.rougoor@hotmail.nl>
2019-03-30 13:17:10 +0100

    Project setup

---
+++ src/main/java/dea/liamsean/App.java
@@ -0,0 +1,13 @@
+package dea.liamsean;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
